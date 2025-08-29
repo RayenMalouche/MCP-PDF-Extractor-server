64e7dc5dccb3321cf1c1465f3bdae35a8013084b
Rayen Malouche <rayenmalouche27@gmail.com>
2025-08-29 14:19:17 +0100

    first commit

---
+++ src/main/java/com/mcp/RayenMalouche/pdf/PDFExtractor/PdfExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.RayenMalouche.pdf.PDFExtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcp/RayenMalouche/pdf/PDFExtractor/PdfExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.RayenMalouche.pdf.PDFExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
